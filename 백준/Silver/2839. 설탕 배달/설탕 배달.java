95abcfa17e0bfdcbe3ea2b33f4f0aca43edb6c07
JIWOO YUN <54807231+YunJiW@users.noreply.github.com>
2022-08-11 18:38:58 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+//탐욕 알고리즘
+
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int check = 0;
+		
+		while(N >= 0)
+		{
+			// 5로 나눠질 경우
+			if( N % 5 == 0) {
+				check += N/5;
+				System.out.println(check);
+				return;
+			}
+			//안 나눠질경우 3을 빼주며 봉투를 1개 늘려줌.
+			N -= 3;
+			check++;
+		}
+		
+		// 안나눠질경우 -1 리턴
+		System.out.println("-1");
+		
+		
+		
+	}
+}
