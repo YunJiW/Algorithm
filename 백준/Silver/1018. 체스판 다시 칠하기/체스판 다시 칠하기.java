21948b2bb203ac657147ca962ad20b38bd5499d1
JIWOO YUN <54807231+YunJiW@users.noreply.github.com>
2022-08-13 14:50:36 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 132 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	// 체스판 만들기용
+	public static boolean[][] chess;
+	// 최소값 계산하기 -> 전체 다 바꿀경우 64
+	public static int min = 64;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer MN = new StringTokenizer(br.readLine(), " ");
+
+		int M = Integer.parseInt(MN.nextToken());
+		int N = Integer.parseInt(MN.nextToken());
+
+		chess = new boolean[M][N];
+
+		// 기본 보드 생성
+		for (int i = 0; i < M; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < N; j++) {
+				// W일경우 true B일경우 false
+				if (str.charAt(j) == 'W') {
+					chess[i][j] = true;
+				} else
+					chess[i][j] = false;
+			}
+		}
+		
+		//경우의  수 계산
+		int row = M - 7;
+		int col = N - 7;
+
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				check(i,j);
+			}
+		}
+		
+		System.out.println(min);
+
+	}
+	//다시 칠해야하는 정사각형 최솟값 계산
+	private static void check(int i, int j) {
+		int row = i+8;
+		int col = j+8;
+		
+		int cnt = 0;
+		//체스의 맨처음 색깔을 받아둠
+		boolean check_WB = chess[i][j];
+		
+		for(int k = i; k < row;k++)
+		{
+			for(int q = j; q< col;q++)
+			{
+				//다를경우 cnt
+				if(chess[k][q] != check_WB)
+				{
+					cnt++;
+				}
+				check_WB = (!check_WB);
+			}
+			check_WB = (!check_WB);
+		}
+		//min 이 더클경우 바꿈
+		cnt = Math.min(cnt,64-cnt);
+		
+		
+		min = Math.min(min, cnt);
+	}
+
+}
