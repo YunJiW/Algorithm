7cc861c187e9673cca0a6d8c39a2a2aa4f309639
JIWOO YUN <54807231+YunJiW@users.noreply.github.com>
2023-03-07 19:25:16 +0900

    [unrated] Title: 덧칠하기, Time: 0.85 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,17 +1,6 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-
-        int max  =0;
-        for(int idx : section){
-            if(max > idx) continue;
-
-            answer+=1;
-            max = idx + m;
-        }
-
-
-
         return answer;
     }
 }
