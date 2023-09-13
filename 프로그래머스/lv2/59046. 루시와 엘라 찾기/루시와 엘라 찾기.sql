6d91080d045d7d4aad595c08b60f68e71ed92f91
JIWOO YUN <54807231+YunJiW@users.noreply.github.com>
2023-09-13 11:52:11 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID,NAME,SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME = 'Lucy' 
+or NAME = 'Pickle'
+or NAME = 'Ella'
+or NAME = 'Rogan'
+or Name = 'Sabrina'
+or Name = 'Mitty'
