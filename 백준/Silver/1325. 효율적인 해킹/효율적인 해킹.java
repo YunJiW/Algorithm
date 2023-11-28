17a51acc2dee2103e9dc3c12b39371dca41cc146
JIWOO YUN <54807231+YunJiW@users.noreply.github.com>
2023-11-28 14:12:23 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2604 ms, Memory: 188084 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,63 +1,90 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int n, m;
-    static List<List<Integer>> list = new ArrayList<>();
-    static boolean[] visited;
-    static int[] hacking;
-    static Queue<Integer> queue = new LinkedList<>();
 
-    public static void main(String[] args) throws Exception {
+    static int N;
+    static int M;
+    static int count;
+    //최대 해킹수 갱신용
+    static int Max  = 0;
+    static List<List<Integer>> Com_number = new ArrayList<>();
+    static boolean[][] visit;
+    //각 숫자별 해킹되는 컴퓨터 수 체크
+    static int[] Com_count;
+
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         StringBuilder sb = new StringBuilder();
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i <= n; i++) list.add(new ArrayList<>());
-        hacking = new int[n + 1];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < m; i++) {
+        Com_count = new int[N + 1];
+        visit = new boolean[N+1][N+1];
+
+        for (int idx = 0; idx <= N; idx++) {
+            Com_number.add(new ArrayList<>());
+        }
+
+        //신뢰 별로 그래프 생성
+        for (int idx = 0; idx < M; idx++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            list.get(a).add(b);
+            int first = Integer.parseInt(st.nextToken());
+            int second = Integer.parseInt(st.nextToken());
+
+            Com_number.get(second).add(first);
         }
+        check();
 
-        int max = 0;
-
-        for (int i = 1; i <= n; i++) {
-            visited = new boolean[n + 1];
-            bfs(i);
-        }
-
-        for (int i : hacking) max = Math.max(max, i);
-
-        for (int i = 1; i <= n; i++) {
-            if (hacking[i] == max) sb.append(i + " ");
-        }
-
-        System.out.println(sb);
-    }
-
-    static void bfs(int v) {
-        queue.clear();
-        queue.add(v);
-        visited[v] = true;
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            for (int next : list.get(current)) {
-                if (!visited[next]) {
-                    queue.add(next);
-                    visited[next] = true;
-                    hacking[next]++;
-                }
+        for (int idx = 1; idx <= N; idx++) {
+            if (Com_count[idx] == Max) {
+                sb.append(idx).append(" ");
             }
         }
+        System.out.println(sb);
 
     }
 
+    private static void check() {
+        Queue<Integer> queue = new ArrayDeque<>();
 
+        for(int i = 1; i <=N;i++){
+            queue.offer(i);
+            while(!queue.isEmpty()){
+
+                int cur = queue.poll();
+                if(visit[i][cur]){
+                    continue;
+                }
+                visit[i][cur] = true;
+                Com_count[i]+=1;
+                for(int num : Com_number.get(cur)){
+                    if(visit[i][num]){
+                        continue;
+                    }
+                    if(num < i){
+                        for(int k = 1; k <=N;k++){
+                            if(visit[i][k])
+                                continue;
+                            if(visit[num][k]){
+                                visit[i][k] = true;
+                                Com_count[i]+=1;
+                            }
+                        }
+                    }else{
+                        queue.offer(num);
+                    }
+                }
+            }
+            Max = Math.max(Max,Com_count[i]);
+        }
+
+    }
 }
+
