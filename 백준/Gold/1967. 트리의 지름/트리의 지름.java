ebaad880e630813064276c8783c899b2210c7f26
JIWOO YUN <54807231+YunJiW@users.noreply.github.com>
2024-01-04 12:55:45 +0900

    [Gold IV] Title: 트리의 지름, Time: 260 ms, Memory: 22896 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -0,0 +1,103 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N = 0;
+    static int result = 0;
+
+    static int max_node = 0;
+
+
+    static ArrayList<Edge>[] nodes;
+
+
+
+    static class Edge{
+        int end;
+        int weight;
+
+        public Edge(int end, int weight) {
+            this.end = end;
+            this.weight = weight;
+        }
+    }
+
+    static class Node{
+        int idx;
+        int cnt;
+
+        public Node(int idx, int cnt) {
+            this.idx = idx;
+            this.cnt = cnt;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N =  Integer.parseInt(br.readLine());
+
+        if(N == 1){
+            System.out.println(0);
+            return;
+        }
+
+        StringTokenizer st;
+
+        nodes = new ArrayList[N+1];
+
+
+
+        for(int idx =1 ;idx <= N;idx++){
+            nodes[idx] = new ArrayList<>();
+        }
+
+        for(int idx = 1; idx <N;idx++){
+            st= new StringTokenizer(br.readLine());
+
+            int first = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            nodes[first].add(new Edge(end,weight));
+            nodes[end].add(new Edge(first,weight));
+        }
+
+        bfs(1);
+        bfs(max_node);
+
+        System.out.println(result);
+
+    }
+
+    public static void bfs(int start){
+
+        boolean visited[] = new boolean[N+1];
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(new Node(start,0));
+        visited[start] = true;
+
+        int max_cnt = 0;
+        while (!queue.isEmpty()){
+            Node cur = queue.poll();
+
+            if(cur.cnt > max_cnt){
+                max_cnt = cur.cnt;
+                max_node = cur.idx;
+            }
+
+            for(Edge e : nodes[cur.idx]){
+                if(!visited[e.end]){
+                    visited[e.end] = true;
+                    queue.add(new Node(e.end,cur.cnt+e.weight));
+
+                }
+            }
+        }
+
+        result = Math.max(result,max_cnt);
+
+    }
+}
