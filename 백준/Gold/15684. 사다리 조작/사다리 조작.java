64986fcd7c7fbc17b3eddd647369f82bfc6c1776
JIWOO YUN <54807231+YunJiW@users.noreply.github.com>
2024-04-04 11:42:06 +0900

    [Gold III] Title: 사다리 조작, Time: 424 ms, Memory: 17344 KB -BaekjoonHub

---
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -0,0 +1,112 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+
+
+    static int ladder[][];
+    static int N;
+    static int M;
+
+    static int H;
+
+    static int res =0;
+
+    static boolean finish = false;
+
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+
+        ladder = new int[H+1][N+1];
+
+        for(int idx = 0; idx < M;idx++){
+            st= new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            ladder[r][c] = 1;
+        }
+        
+        //핵심 로직
+        //1. 사다리 놓기
+        //2. 원하는 결과가 나오는지 체크
+        for(int idx = 0; idx <= 3 ;idx++){
+            res = idx;
+            comb(1,0);
+            if(finish) break;
+        }
+
+        if(finish) {
+            System.out.println(res);
+            return;
+        }
+
+        System.out.println(-1);
+
+
+    }
+
+    private static void comb(int nextrow, int cnt) {
+        if (finish) return;
+        if(cnt == res){
+            //사다리 체크 진행
+            if(check()){
+                finish = true;
+            }
+
+            return;
+        }
+
+        for(int row =nextrow; row <=H;row++){
+            for(int c = 1;c <N;c++){
+                //현재 자리에 사다리가 있는경우
+                if(ladder[row][c] == 1){
+                    continue;
+                }
+                //내 옆으로 나까지 연결되는 경우
+                if(ladder[row][c-1] == 1){
+                    continue;
+                }
+                //내 옆에 사다리가 있는경우(연속으로 붙가능하니까)
+                if(ladder[row][c+1] ==1){
+                    continue;
+                }
+                ladder[row][c] = 1;
+                comb(row,cnt+1);
+                ladder[row][c] = 0;
+            }
+        }
+    }
+
+    private static boolean check() {
+        for(int idx =1 ;idx <=N;idx++){
+            int curPos = idx;
+            int start = 1;
+
+            while(start <=H){
+                if(ladder[start][curPos] == 1){
+                    curPos+=1;
+                    start+=1;
+                }else if(ladder[start][curPos-1] == 1){
+                    curPos-=1;
+                    start+=1;
+                }else{
+                    start+=1;
+                }
+
+            }
+            if(idx != curPos)
+                return false;
+        }
+        return true;
+    }
+}
